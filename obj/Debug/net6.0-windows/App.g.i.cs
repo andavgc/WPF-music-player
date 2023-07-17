661ef90d7a19d218872000820ed74ffc75aaf12c
lknknm <108239558+lknknm@users.noreply.github.com>
2023-07-17 19:22:04 -0300

    Fix Assets Path

--- obj/Debug/net6.0-windows/App.g.i.cs
+++ obj/Debug/net6.0-windows/App.g.i.cs
@@ -44,7 +44,7 @@ namespace WPF_music_player {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.19.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace WPF_music_player {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.19.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public static void Main() {
             WPF_music_player.App app = new WPF_music_player.App();
             app.InitializeComponent();
